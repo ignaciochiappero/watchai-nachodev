42bbc4682b8bd56fd519ef1dce9c17c87feb7cda
nachochiappero <ignaciogabrielchiappero@gmail.com>
2024-07-25 08:37:29 -0300

    arreglo redes sociales

--- components/social-links.tsx
+++ components/social-links.tsx
@@ -1,21 +1,37 @@
-"use client"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faYoutube, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+"use client";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faChrome,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 const SocialMediaLinks = () => {
-    return (
-        <div className='flex flex-row gap-4'>
-            <a href="https://github.com/KoushikJit" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faGithub} size="2x" />
-            </a>
-            <a href="https://www.youtube.com/channel/UCfKRZUU78XIcQvh1_QsdQyA" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faYoutube} size="2x" />
-            </a>
-            <a href="https://www.linkedin.com/in/koushik-sarkar-iimc/" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faLinkedin} size="2x" />
-            </a>
-        </div>
-    );
+  return (
+    <div className="flex flex-row gap-4">
+      <a
+        href="https://github.com/ignaciochiappero"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faGithub} size="2x" />
+      </a>
+      <a
+        href="https://www.nachochiapperodev.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faChrome} size="2x" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/ignacio-chiappero-129360228/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faLinkedin} size="2x" />
+      </a>
+    </div>
+  );
 };
 
 export default SocialMediaLinks;
